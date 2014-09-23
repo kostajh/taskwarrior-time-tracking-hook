932dfe50b5ec6036d6daeb1818d30d5bfb33fc0d
Kosta Harlan <kosta@kostaharlan.net>
2014-09-23 09:47:13 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taskwarrior-time-tracking-hook',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/coddingtonbear/taskwarrior-time-tracking-hook',
     description=(
         'Track your time in a UDA in taskwarrior'
